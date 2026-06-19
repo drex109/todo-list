818db9071c2412b65437a829f54a8f46ac23a349
drex109 <96746274+drex109@users.noreply.github.com>
2026-06-19 15:18:05 -0600

    test commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./style.css";
+
+const createTodo = (title, description, dueDate, priority) => {
+    return { 
+        title,
+        description,
+        dueDate,
+        priority
+    }
+}
+
+const lawn = createTodo('Mow Lawn', 'Lawn is overgrown and need mowing', '6/20/26', "high");
+console.log(lawn);
+
+const deleteTodo = () => {
+    
+}
+
+const editTodo = () => {
+
+}
+
+
+const createProject = () => {
+
+}
+
+const displayProjects = () => {
+
+    displayTodo = () => {
+        
+    }
+
+}
